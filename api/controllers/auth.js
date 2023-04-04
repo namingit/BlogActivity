d4826e4fd8e2b640951de31999e53a8c35cc7d24
namingit <mundra.namita@gmail.com>
2023-04-04 17:36:25 +0530

    initial commit

---
+++ api/controllers/auth.js
@@ -0,0 +1,42 @@
+import {db} from "../db.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register=(req,res)=>{
+ const q= "SELECT * FROM users WHERE email=? OR username=?";
+    db.query(q,[req.body.email,req.body.username],(err,data)=>{
+        if(err)return res.json(err);
+        // console.log(data);
+        if(data.length)return res.status(409).json("user already exists:");
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+        const q="INSERT INTO users(`username`,`email`,`password`) VALUES (?)"
+        const values=[
+            req.body.username,
+            req.body.email,
+            hash,
+        ]
+        db.query(q,[values],(err,data)=>{
+            if(err) return res.json(err);
+            return res.status(200).json("user created");
+        })
+    })
+}
+export const login=(req,res)=>{
+    const q="SELECT * FROM users WHERE username=?";
+    db.query(q, [req.body.username], (err,data)=>{
+        if (err) return res.json(err);
+        if(data.length===0) return res.status(404).json("user not found");
+        const isPwdCrct=bcrypt.compareSync(req.body.password, data[0].password);
+        if(!isPwdCrct) return res.status(400).json("username or password not correct");
+        const token=jwt.sign({id:data[0].id},"jwtkey");
+        const {password, ...rest}=data[0]
+        res.cookie("access_token", token, {httpOnly:true}).status(200).json(rest)
+    })
+}
+export const logout=(req,res)=>{
+    res.clearCookie("access_token",{
+        sameSite : "none",
+        secure : true
+    }).status(200).json("user has logged out");
+}
